6582cc4de3c301c15646d939b1f7b6345b9fd21a
Oukangneng <oaoconnell@gmail.com>
2025-03-26 20:11:54 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,7 @@ const config = {
             id: 'intro',
             title: 'Tracking Taiwan’s Undersea Cable Incidents | By Owen OConnell',
             image: './data/taiwan_cables_map.jpg', 
-            description: `
-                Taiwan relies on a network of undersea cables for internet connectivity. These cables are frequently damaged, 
-                with incidents often linked to Chinese vessels. This visualization highlights key incidents and their locations.
-                <br><br>Sources: 
-                <a href="https://www2.telegeography.com" target="_blank">TeleGeography</a>, 
-                <a href="https://www.ncc.gov.tw/" target="_blank">Taiwan’s National Communications Commission</a>
-            `,
+            description: `aabccd',
             location: {
                 center: [121.5, 23.5], 
                 zoom: 6,
@@ -31,10 +25,7 @@ const config = {
             id: 'incident-matsu',
             title: 'Matsu Cable 1 Incident',
             image: './data/matsu_incident.jpg',
-            description: `
-                On February 2, 2023, a Chinese fishing vessel cut the Matsu Cable 1 near the Matsu Islands. 
-                This caused a major internet outage for the islands, lasting several weeks.
-            `,
+            description: `aaa',
             location: {
                 center: [119.97, 26.15], 
                 zoom: 10,
@@ -81,16 +72,7 @@ const config = {
         {
             id: 'conclusion',
             title: 'Conclusion',
-            description: `
-                <span style="font-size:24px">Taiwan’s undersea cables face growing threats, disrupting global internet traffic 
-                and raising security concerns.</span>
-                <br><br>
-                <span style="font-size:18px"><i>Learn More:</i></span><br>
-                <ul>
-                    <li><a href="https://www2.telegeography.com" target="_blank">TeleGeography’s Submarine Cable Map</a></li>
-                    <li><a href="https://www.ncc.gov.tw/" target="_blank">Taiwan’s National Communications Commission</a></li>
-                </ul>
-            `,
+            description: `aa `,
             location: {
                 center: [121.5, 23.5],
                 zoom: 6,
