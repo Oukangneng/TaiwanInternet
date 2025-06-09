92897f27c540a22c266a1e4d29cab5ab699394e3
Oukangneng <oaoconnell@gmail.com>
2025-06-09 16:45:45 -0400

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ const config = {
             image: './data/taiwan_cables_map.jpg',
             description: 'This visual timeline will guide you through incidents of undersea cables being severed between Taiwan and other regions. (Scroll ⤓ to begin exploring the timeline)',
             location: {
-                center: [123.5, 23.5],
-                zoom: 8,
+                center: [122.5, 23.5],
+                zoom: 7,
                 pitch: 0,
                 bearing: 0
             },
