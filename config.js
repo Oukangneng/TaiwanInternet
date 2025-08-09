427f3eeb1cb2d281be0ac4452497f213dd4bc1e5
Oukangneng <oaoconnell@gmail.com>
2025-08-09 15:54:21 -0400

    Update config.js

--- config.js
+++ config.js
@@ -71,13 +71,6 @@ const config = {
         data.features.forEach(feature => {
           const el = document.createElement('div');
           el.className = 'red-marker';
-
-          // Highlight the TPE marker by coordinates with a special class
-          const coords = feature.geometry.coordinates;
-          if (coords[0] === 121.7892 && coords[1] === 25.3787) {
-            el.classList.add('marker-tpe');
-          }
-
           el.style.width = '24px';
           el.style.height = '24px';
           el.style.borderRadius = '50%';
@@ -95,7 +88,7 @@ const config = {
           `;
 
           const marker = new mapboxgl.Marker(el)
-            .setLngLat(coords)
+            .setLngLat(feature.geometry.coordinates)
             .setPopup(
               new mapboxgl.Popup({ offset: 25 }).setHTML(popupHTML)
             )
